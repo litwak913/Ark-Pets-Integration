7fd3a3f860928663c78c94860c81220b8742b8a1
litwak913 <litwak913@outlook.com>
2025-07-06 15:17:45 +0800

    chore: remove

--- README.md
+++ README.md
@@ -6,5 +6,3 @@ ArkPets 的桌面集成，为 ArkPets 提供了查询，控制窗口之类的接
 
 * GNOME 集成扩展
 * KWin 集成插件
-* Rust Bootstrapper
-* macOS Native（已弃用）
--- bootstrapper/src/consts.rs
+++
@@ -1,3 +0,0 @@
-pub const LIBS_DIR: &str = "app";
-pub const RUNTIME_DIR: &str = "runtime";
-pub const PLUGINS_DIR: &str = "plugins";
