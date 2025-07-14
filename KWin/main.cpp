17bc0030fd8892681536f849739497cf4017580d
litwak913 <litwak913@outlook.com>
2025-07-14 20:20:05 +0800

    fix: moc namespace

--- KWin/main.cpp
+++ KWin/main.cpp
@@ -15,7 +15,7 @@ class KWIN_EXPORT ArkPetsIntegrationFactory : public PluginFactory
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID PluginFactory_iid FILE "metadata.json")
-    Q_INTERFACES(PluginFactory)
+    Q_INTERFACES(KWin::PluginFactory)
 public:
     explicit ArkPetsIntegrationFactory() = default;
     std::unique_ptr<Plugin> create() const override
